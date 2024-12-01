f325359bec0e80d9bee2ac3e54bd1c22f4185170
apaal22 <bratmotik@gmail.com>
2024-12-01 16:43:13 +0200

    Add files via upload

---
+++ python program/lesson1-2.py
@@ -0,0 +1,72 @@
+print("hello world!")
+
+#тип зміноЇ int, ціли числа
+number = 10
+
+#Тип зміной Float, не цілі числа
+price = 9.99
+
+#Тип зміноЇ String, рядок або текст
+name = "Matvii"
+
+#Тип  зміної bool(boolean),True або False(1 або 0)
+light = True
+
+banan = 2
+apple =4
+
+cocacola = 0.5
+sprite = 1.5
+
+name = "матвій"
+surname = "Тимофій"
+
+string = "алока"
+spring = "абоба"
+
+light = True 
+night = False
+
+print(name)
+print(surname)
+
+print(type(night))
+print(type(spring))
+
+print(type(sprite))
+print(type(banan)
+
+# tymofii = 5
+# matvii = 4
+# if timofii < 6:
+#     print("2 point")
+# elif matvii < 6:
+#     print("2 points")
+# else:
+#     print("boys have 12 points")
+
+andrey = 9
+ana = 6
+
+if andrey < 8:
+    print("win")
+elif ana < 8:
+    print("Louse")
+else:
+    print("They have good")
+
+ 
+ for i in range(10):
+  print('hello world!')
+
+a = 5 
+
+while a < 10 
+   a += 1
+   print(a)
+  
+b = 15
+
+while b < 20
+ b += 1:
+ print(b)
